d5008c3552f304ffd6dbd2acf986d36185a98ce4
kirthiSaiT <kirthisai251@gmail.com>
2024-08-24 20:44:36 +0530

    commit

--- index.js
+++ index.js
@@ -5,19 +5,68 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
     res.status(201).json({
         message: "THE SERVER IS UP AND RUNNING",
         data:"hey",
     });
 });
 
+
+
 app.get("/users",(req,res)=>{
     res.status(200).json({
         success:true,
         data:users
     })
+});
+
+app.get('/users/:id', (req, res) => {
+    const {id}=req.params;
+    console.log(req.params);
+    const user = users.find((each)=>each.id===id);
+
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "User Doesn't Exist!!"
+        });
+    }
+    return res.status(200).json({
+        success: true,
+        message: "User Found",
+        data: user,
+    });
+    
+});
+
+app.post("/users",(req,res)=>{
+    const{id,name,surname,email,subscriptionType,subscriptionDate}=req.body;
+
+    const user=users.find((each)=>each.id===id);
+    
+    if(user){
+        return res.status(404).json({
+            success:false,
+            message:"User with the ID Exsists",
+        })
+    }
+
+    users.push({
+        id,
+        name,
+        surname,
+        email,
+        subscriptionType,
+        subscriptionDate,
+    });
+    return res.status(201).json({
+        success:true,
+        message:"User Added",
+        data:users,
+    });
 })
+
 
 
 
